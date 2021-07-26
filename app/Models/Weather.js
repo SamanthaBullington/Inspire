90e91987dc56cf482727ce31fd12e506ea7b2cd2
SamanthaBullington <Samantha.J.Bullington@gmail.com>
2021-07-26 13:45:39 -0600

    update

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -2,15 +2,19 @@ export default class Weather {
   constructor(data) {
     this.name = data.name
     this.temp = data.main.temp
+    this.celsius = data.main.temp - 273.15
+    this.fahrenheit = data.main.temp * (9 / 5) - 459.67
+    this.isfahrenheit = true
   }
-
   get Template() {
     return `
     <div>${this.temp} in ${this.name}</div>
-    
     `
   }
 }
+
+//content editable
+
 // <button id="tempchange" onclick = "this.temperatureConverter()></button>
 // <p>Fahrenheit:</p> <span id="outputFahrenheit"></span>
 
